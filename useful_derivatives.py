5579501c45acbe1c714c09a95761c4e1fc060fda
TyrannoJ <julius.megerle@outlook.de>
2026-08-24 19:06:57 +0200

    turning points work

--- useful_derivatives.py
+++ useful_derivatives.py
@@ -1,16 +1,18 @@
 from manim import *
 import sympy as sp
 
-functions=[]
+final_coefficients=[]
+final_powers=[]
 null_points=[]
 extreme_points=[]
 turning_points=[]
-
+coefficients=[1,2,7]
+powers=[5,3,2]
+zeros_x_coords=[[],[],[]]
 class Derivatives(Scene):
     
     def construct(self):
-        coefficients=[1,2,7]
-        powers=[5,3,2]
+        
         
         ax=Axes(
             (-10,10,2),
@@ -28,9 +30,11 @@ class Derivatives(Scene):
                 "include_numbers": True,
                 "include_tip": False,
                 
-    },
-        )
-        
+            },
+                )
+
+        #Add functions
+        self.create_functions()
         self.add(ax)
         curves=[]
         colors=color_gradient([BLUE,GREEN,RED],3)
@@ -44,13 +48,13 @@ class Derivatives(Scene):
         
         #self.add(tex)
         for i in range(0,3):
-            functions.append(coefficients.copy())
-            functions.append(powers.copy())
+            coefficients=final_coefficients[i]
+            powers=final_powers[i]
             if i==0:
                 op=1
             else:
                 op=0.5
-            cur=ax.plot(lambda x:self.final_function(x,coefficients,powers),color=colors[i],x_range=(-3,3)).set_stroke(opacity=op)
+            cur=ax.plot(lambda x:self.final_function(x,coefficients,powers),color=colors[i],x_range=(-10,10)).set_stroke(opacity=op)
             curves.append(cur)
             if i==0:
                 text=rf"f(x)="
@@ -83,17 +87,17 @@ class Derivatives(Scene):
                 texts.append(tec.copy())
            # print(self.final_function(2,coefficients,powers))
             #print(self.zero_points(coefficients,powers))
-            solutions=self.zero_points(coefficients,powers,i)
             
+            solutions=zeros_x_coords[i]
             self.add(cur,tec)
             #self.play(
                 #Create(cur),
                 #Transform(
-                        #texts[i],
-                        #tec
-                    #),
-                #run_time=3
-            #)
+                 #       texts[i],
+                 #       tec
+                 #   ),
+               # run_time=3
+           # )
             if solutions:
                 for s in solutions:
                     so=s.evalf(3)
@@ -106,27 +110,52 @@ class Derivatives(Scene):
                 null_points.append(points)
                 self.add(points)
             
-            for i in range(0,len(powers)):
-                coefficients[i],powers[i]=self.make_derivative(coefficients[i],powers[i])
-        
+            
+        #Extreme Points
         title=Text("Extreme Points").scale(0.5).to_corner(UR)
-        
+
+        self.wait(2)
         self.add(title)
         
         extremes=VGroup(
-            *[Dot(ax.c2p(ex[0], ex[1]), color=BLUE) for ex in extreme_points]
+            *[Dot(ax.c2p(ex[0], ex[1]), color=colors[0]) for ex in extreme_points]
         )
         extreme_labels=Group(
-            *[MathTex(rf"{ex[2]} ({ex[0]} ,{ex[1]})",color=BLUE).scale(0.5).to_corner(UR).shift(DOWN*(i/2+0.5)) for i,ex in enumerate(extreme_points)]
+            *[MathTex(rf"{ex[2]} ({ex[0]} ,{ex[1]})",color=colors[0]).scale(0.5).to_corner(UR).shift(DOWN*(i/2+0.5)) for i,ex in enumerate(extreme_points)]
         )
         for n in null_points:
             self.remove(n)
         self.remove(curves[2])
         self.add(extremes,extreme_labels)
+
+        self.wait(2)
+
+        #Turning Points
+        tu=Tex("Turning Points").scale(0.7).to_corner(DR).shift(2.5*UP)
+        self.add(tu)
+        turns=VGroup(
+            *[Dot(ax.c2p(tu[0], tu[1]), color=colors[0]) for tu in turning_points]
+        )
+        turning_labels=Group(
+            *[MathTex(rf"{tu[2]} ({tu[0]} ,{tu[1]})",color=colors[0]).scale(0.5).to_corner(DR).shift(UP*(2-(i/2))) for i,tu in enumerate(turning_points)]
+        )
+        self.add(curves[2])
+        self.remove(curves[1],extremes)
+        self.add(turns,turning_labels)
         self.wait(3)
 
 
-     
+    def create_functions(self):
+        for i in range(0,4):
+            final_coefficients.append(coefficients.copy())
+            final_powers.append(powers.copy())
+            solutions=self.zero_points(coefficients,powers,i)
+            if solutions:
+                for s in solutions:
+                    zeros_x_coords[i].append(s)
+            for i in range(0,len(powers)):
+                coefficients[i],powers[i]=self.make_derivative(coefficients[i],powers[i])
+        
     def make_derivative(self,co,pow):
         co=co*pow
         pow-=1
@@ -145,14 +174,16 @@ class Derivatives(Scene):
                 a=a+co[i]*x**po[i]
         
         points=sp.solveset(a,x,domain=sp.S.Reals)
+        
+        
         if iteration==1 and points:
             
             for p in points:
-                extreme_points.append([p.evalf(3),self.final_function(p.evalf(3),functions[0],functions[1])])
+                extreme_points.append([p.evalf(3),self.final_function(p.evalf(3),final_coefficients[0],final_powers[0])])
         if iteration==2:
             for p in extreme_points:
                 value=""
-                second_derivative_value=self.final_function(p[0].evalf(3),functions[4],functions[5])
+                second_derivative_value=self.final_function(p[0].evalf(3),final_coefficients[2],final_powers[2])
                 if second_derivative_value==0:
                     value="TEP"
                 if second_derivative_value>0:
@@ -160,6 +191,20 @@ class Derivatives(Scene):
                 if second_derivative_value<0:
                     value="HOP"
                 p.append(value)
+            for p in points:
+                turning_points.append([p.evalf(3),self.final_function(p.evalf(3),final_coefficients[0],final_powers[0])])
+        if iteration==3:
+            for p in turning_points:
+                value=""
+                third_derivative_value=self.final_function(p[0].evalf(3),final_coefficients[3],final_powers[3])
+                if third_derivative_value==0:
+                    value="?"
+                if third_derivative_value>0:
+                    value="R-L"
+                if third_derivative_value<0:
+                    value="L-R"
+                p.append(value)
+            
 
         return(points)
     
